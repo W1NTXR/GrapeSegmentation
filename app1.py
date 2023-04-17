0615c841669f1900aadd19a544e8102b849c889d
rishabhyaduwanshi <caml20053@glbitm.ac.in>
2023-04-17 19:33:11 +0530

    contour code with boundry image

--- app1.py
+++ app1.py
@@ -8,9 +8,11 @@ temp=npzfile[ls[0]]
 # Convert the contours to bounding boxes and normalize the coordinates
 bounding_boxes = []
 print(temp.shape)
+image=cv2.imread('Dataset/CDY_2015.jpg')
+cont=[]
 for i in range(temp.shape[2]):
     class_index = 0
-
+    
     # Load the numpy array with grape pixels
     grape_array = temp[:,:,i]
 
@@ -24,8 +26,8 @@ for i in range(temp.shape[2]):
     # Find the contours of the grape regions
     contours, hierarchy = cv2.findContours(grape_image, cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_SIMPLE)
     # print(contours)
-    
     for contour in contours:
+        cont.append(contour)
         x,y,w,h = cv2.boundingRect(contour)
         x_center = (x + w/2) / grape_image.shape[1]
         y_center = (y + h/2) / grape_image.shape[0]
@@ -42,7 +44,8 @@ for i in range(temp.shape[2]):
 #     print('|')   
 
 # Define the class index for grape
-
+cv2.drawContours(image,cont,-1,(0,0,255),2)
+cv2.imwrite('Contours.jpg',image)
 
 # Write the bounding boxes to a YOLOv7 format text file
 with open('grape_bounding_boxes.txt', 'w') as f:
@@ -59,3 +62,4 @@ with open('grape_bounding_boxes.txt', 'w') as f:
 
 # # Save the image to disk
 # cv2.imwrite('output.jpg', img_cv2)
+
